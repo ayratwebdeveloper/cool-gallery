3dbd48bcf26d613e82bd02aa0c80116a60e49490
ayratdeveloper <ayratdeveloper@gmail.com>
2023-05-22 18:59:02 +0400

    working slider and adaptation to screen size

--- src/javascript/code.js
+++ src/javascript/code.js
@@ -1,3 +1,16 @@
 const sliderMain = new Swiper('.slider_main', {
-	
+	freeMode: true,
+	centeredSlides: true,
+	// screen size adaptation
+	breakpoints: {
+		0 :{
+			slidesPerView: 2.5,
+			spaceBetween: 20
+		},
+
+		680: {
+			slidesPerView: 3.5,
+			spaceBetween: 60
+		}
+	}
 })

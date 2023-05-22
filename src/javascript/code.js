747391999571b57a563b83d2e3dc93e4c44cee0b
ayratdeveloper <ayratdeveloper@gmail.com>
2023-05-23 01:32:43 +0400

    completed project

--- src/javascript/code.js
+++ src/javascript/code.js
@@ -1,11 +1,13 @@
+// Первый слайдер
 const sliderMain = new Swiper('.slider_main', {
 	freeMode: true,
 	centeredSlides: true,
 	parallax: true,
-	// mouse scrolling
+	// Скролл мышью
 	mousewheel: true,
-	// screen size adaptation
+	// Адаптация под размер экрана
 	breakpoints: {
+
 		0 :{
 			slidesPerView: 2.5,
 			spaceBetween: 20
@@ -18,6 +20,7 @@ const sliderMain = new Swiper('.slider_main', {
 	}
 })
 
+// Второй слайдер
 const sliderBig = new Swiper('.slider_big', {
 	centeredSlides: true,
 	parallax: true,
@@ -25,4 +28,19 @@ const sliderBig = new Swiper('.slider_big', {
 	slidesPerView: 3.5,
 })
 
+// Привязка слайдеров
 sliderMain.controller.control = sliderBig
+
+// Прохожу по всем item'ом и выбираю что с ним сделать
+document.querySelectorAll('.slider__item').forEach(item => {
+	item.addEventListener('click', (event) =>{
+		 item.classList.toggle('extendedSlide')
+		})
+})
+
+// Триггер на столкновение контейнеров
+let synopsis = document.querySelector('.synopsis')
+sliderMain.on('slideChange', event =>{
+	sliderMain.activeIndex > 0 ? synopsis.classList.add('pinch'):
+	synopsis.classList.remove('pinch')
+})
